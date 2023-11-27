e4ae28c17833555e6dda3b06138dee47f3415d0d
elnfach <vpetuhov160@gmail.com>
2023-11-27 18:06:34 +0800

    Update INT.cpp

--- ООП/INT.cpp
+++ ООП/INT.cpp
@@ -3,59 +3,37 @@
 
 INT::INT()
 {
-	counter = 0;
-	count = 0;
-	count_buf = 0;
 	m_size = m_array_size - 1;
 	m_container = new unsigned char[m_array_size]();
 }
 
-INT::INT(INT* param)
+INT::INT(INT& param)
 {
-	counter = 0;
-	count = 0;
-	count_buf = 0;
-	m_size = param->m_size;
+	m_size = param.m_size;
 
 	delete[] m_container;
 	m_container = new unsigned char[m_array_size]();
 
-	memcpy(m_container, param->m_container, m_array_size);
+	memcpy(m_container, param.m_container, m_array_size);
 }
 
 INT::INT(const int param)
 {
-	counter = 0;
-	count = 0;
-	count_buf = 0;
 	int _param = param;
-	int buff = _param;
-	int size = 1;
 
-	m_size = 2;
+	if(_param > 9)
+		throw ()
 
-	while (buff > 9)
-	{
-		size += 1;
-		buff /= 10;
-	}
-
-	delete[] this->m_container;
+	delete[m_array_size] this->m_container;
 	m_container = new unsigned char[m_array_size]();
 
 	m_size = m_array_size - size;
 	m_container = this->m_container;
-
-	for (int i = m_array_size - 1; i >= m_size; i--)
-	{
-		m_container[i] = _param % 10;
-		_param /= 10;
-	}
 }
 
 INT::~INT()
 {
-	delete[] m_container;
+	delete[m_array_size] m_container;
 }
 
 void INT::screen()
@@ -103,39 +81,13 @@ INT INT::operator+(const INT& param)
 			}
 		}
 	}
-	counter += 1;
 	return temp;
-}
-
-bool INT::operator[](const int param)
-{
-	for (int i = m_array_size - 1; i >= m_size; i--)
-	{
-		if (m_container[i] == 0)
-		{
-			count += 1;
-			if (count > count_buf)
-			{
-				count_buf = count;
-				std::cout << "[" << count_buf << " - " << counter << "]";
-			}
-			if (count == param)
-			{
-				return true;
-			}
-		}
-		else
-		{
-			count = 0;
-		}
-	}
-	return false;
 }
 
 void INT::operator=(const INT& param)
 {
 	m_size = param.m_size;
-	delete[]m_container;
+	delete[m_array_size] m_container;
 	m_container = new unsigned char[m_array_size];
 	memcpy(m_container, param.m_container, m_array_size);
 }
